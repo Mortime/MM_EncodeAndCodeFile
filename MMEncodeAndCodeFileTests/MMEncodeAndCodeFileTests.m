91604a47e0826c407c348f669afbd8ea044e1cf1
Mortime <Mortimey@163.com>
2016-08-17 16:46:03 +0800

    Initial Commit

---
+++ MMEncodeAndCodeFileTests/MMEncodeAndCodeFileTests.m
@@ -0,0 +1,39 @@
+//
+//  MMEncodeAndCodeFileTests.m
+//  MMEncodeAndCodeFileTests
+//
+//  Created by Mortimey on 16/8/17.
+//  Copyright © 2016年 Mortimey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MMEncodeAndCodeFileTests : XCTestCase
+
+@end
+
+@implementation MMEncodeAndCodeFileTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
